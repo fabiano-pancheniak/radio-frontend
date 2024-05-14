2ab06ba338b024559942e4f71b10ef2a20dd7c20
Fabiano <fabiano.ti@unc.br>
2024-05-13 22:00:56 -0300

    feat: snackbar

--- src/app/components/admin/servico/servico-form/servico-form.component.ts
+++ src/app/components/admin/servico/servico-form/servico-form.component.ts
@@ -3,17 +3,30 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ServicoService } from '../../../../services/ordem-servico/servico.service';
 import { Servico } from '../../../../interfaces/servico';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { CommonModule } from '@angular/common';
+import {
+  MatSnackBar,
+  MatSnackBarAction,
+  MatSnackBarActions,
+  MatSnackBarLabel,
+  MatSnackBarRef,
+} from '@angular/material/snack-bar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-servico-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatSnackBarModule, CommonModule, MatFormFieldModule, FormsModule, MatInputModule, MatButtonModule],
   providers: [{provide: ServicoService}],
   templateUrl: './servico-form.component.html',
   styleUrl: './servico-form.component.scss'
 })
 export class ServicoFormComponent {
-  constructor(private route: ActivatedRoute, private router: Router){
+  constructor(private route: ActivatedRoute, private router: Router, private _snackBar: MatSnackBar){
     if(this.servicoId){
       this.servicoService.getServico(this.servicoId).subscribe(res => {
         this.servicoForm.controls['descricao'].setValue(res.descricao)
@@ -35,13 +48,27 @@ export class ServicoFormComponent {
     if(this.servicoId){
       this.servicoService.updateServico(this.servicoId, this.servicoForm.value).subscribe(
         res => {
+          this._snackBar.open("Serviço atualizado atualizado", '', {
+            duration: 3000,
+            horizontalPosition: 'right',
+            verticalPosition: 'bottom',
+          })
           this.router.navigate(['admin/servico'])
         })
         return
       }
       this.servicoService.createServico(this.servicoForm.value).subscribe(res => {
+        this._snackBar.open("Serviço cadastrado", '', {
+          duration: 3000,
+          horizontalPosition: 'right',
+          verticalPosition: 'bottom',
+        })
         this.router.navigate(['admin/servico'])
       })
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action);
   }
 
 
